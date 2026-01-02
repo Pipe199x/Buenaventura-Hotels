a32f9b11327970af45c2550b7bc6c1338c05a4cb
Pipe199x <duqueandres800@gmail.com>
2026-01-02 17:43:10 -0500

    feat(frontend): initialize Angular app

---
+++ frontend/hotel-insights/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './core/auth.service';
+
+@Component({
+  selector: 'app-root',
+  template: `<router-outlet></router-outlet>`
+})
+export class AppComponent implements OnInit {
+  constructor(private auth: AuthService) {}
+  ngOnInit() { this.auth.init(); }
+}
