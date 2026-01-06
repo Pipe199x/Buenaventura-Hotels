6de3da72d69042cb3b466f6db8ac752f6410ff99
Pipe199x <duqueandres800@gmail.com>
2026-01-05 20:02:16 -0500

    chore: remove deprecated files and cleanup repository

--- src/db.py
+++
@@ -1,4 +0,0 @@
-"""Backward compatibility: re-export from infrastructure layer."""
-from .infrastructure.database import get_db_url, get_engine, ping
-
-__all__ = ["get_db_url", "get_engine", "ping"]
