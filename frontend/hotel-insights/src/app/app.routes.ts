7f337fb9fb7af381f9447943c472240cdd21fc87
Pipe199x <duqueandres800@gmail.com>
2026-01-06 21:53:48 -0500

    feat(router): configure application routes and providers

--- frontend/hotel-insights/src/app/app.routes.ts
+++ frontend/hotel-insights/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './features/home/home';
+import { About } from './features/about/about';
+
+import { Hotels } from './features/hotels/hotels';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'hotels', component: Hotels },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: '**', redirectTo: '' }
+];
