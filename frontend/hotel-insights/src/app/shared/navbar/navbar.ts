934b49791fb56d08d182d353af0b28f880e1eb51
Pipe199x <duqueandres800@gmail.com>
2026-01-07 21:09:30 -0500

    feat(ui): add navbar component

---
+++ frontend/hotel-insights/src/app/shared/navbar/navbar.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../core/auth.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+  session$!: Observable<any>;
+
+  constructor(private auth: AuthService, private router: Router) {
+    this.session$ = this.auth.session$;
+  }
+
+  async logout() {
+    await this.auth.signOut();
+    this.router.navigateByUrl('/login');
+  }
+}
