0c01b9fe491a6b7f5e2e6e8eba5b257fdd702fbf
Pipe199x <duqueandres800@gmail.com>
2026-01-07 21:09:37 -0500

    feat(ui): render navbar in app shell

--- frontend/hotel-insights/src/app/app.component.ts
+++ frontend/hotel-insights/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './core/auth.service';
+import { Navbar } from './shared/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`
+  imports: [RouterOutlet, Navbar],
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent implements OnInit {
   constructor(private auth: AuthService) {}
-  ngOnInit() { this.auth.init(); }
+
+  ngOnInit() {
+    this.auth.init();
+  }
 }
