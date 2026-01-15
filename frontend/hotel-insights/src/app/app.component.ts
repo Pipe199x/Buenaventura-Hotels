344fdff01dd09db395d624d8a648eaaba5a90128
Pipe199x <duqueandres800@gmail.com>
2026-01-15 17:54:24 -0500

    refactor(app): adjust root component layout

--- frontend/hotel-insights/src/app/app.component.ts
+++ frontend/hotel-insights/src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './core/auth.service';
-import { Navbar } from './shared/navbar/navbar';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
-  template: `
-    <app-navbar></app-navbar>
-    <router-outlet></router-outlet>
-  `
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent implements OnInit {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private translate: TranslateService) {
+    const saved = localStorage.getItem('lang') || 'es';
+    this.translate.setDefaultLang('es');
+    this.translate.use(saved);
+  }
 
   ngOnInit() {
     this.auth.init();
