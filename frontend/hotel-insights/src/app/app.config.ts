e2e6c92919feb5c7a14003b152ac2899f248ca62
Pipe199x <duqueandres800@gmail.com>
2026-01-30 06:29:22 -0500

    config(app): configure router and http client providers

--- frontend/hotel-insights/src/app/app.config.ts
+++ frontend/hotel-insights/src/app/app.config.ts
@@ -1,24 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
 import { routes } from './app.routes';
-
-import { provideHttpClient } from '@angular/common/http';
-
-import { provideTranslateService } from '@ngx-translate/core';
-import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
 
-    provideTranslateService({
-      loader: provideTranslateHttpLoader({
-        prefix: '/assets/i18n/',
-        suffix: '.json',
-      }),
-      lang: 'es',
-      fallbackLang: 'es',
-    }),
+    // SSR recomendado (quita el warning NG02801)
+    provideHttpClient(withFetch()),
   ],
 };
