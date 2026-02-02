97e5f8d267f9bfae813704202da2feb4dd01c8d1
Pipe199x <duqueandres800@gmail.com>
2026-02-02 09:32:59 -0500

    feat(app): configure providers (http fetch, echarts core, auth init)

--- frontend/hotel-insights/src/app/app.config.ts
+++ frontend/hotel-insights/src/app/app.config.ts
@@ -1,14 +1,48 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
+import { provideRouter, withRouterConfig } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { AuthService } from './core/auth.service';
+
+/* =======================
+   NGX-ECHARTS (CORE)
+======================= */
+import { provideEchartsCore } from 'ngx-echarts';
+
+import * as echarts from 'echarts/core';
+import { BarChart } from 'echarts/charts';
+import { GridComponent, TooltipComponent, LegendComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+
+echarts.use([BarChart, GridComponent, TooltipComponent, LegendComponent, CanvasRenderer]);
+
+export function initAuth(auth: AuthService) {
+  return () => auth.init();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withRouterConfig({
+        // ✅ si haces click en Inicio estando en /home, recarga el componente
+        onSameUrlNavigation: 'reload',
+        paramsInheritanceStrategy: 'always',
+      })
+    ),
 
-    // SSR recomendado (quita el warning NG02801)
     provideHttpClient(withFetch()),
+
+    // ✅ ECharts
+    provideEchartsCore({ echarts }),
+
+    // ✅ Auth init
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initAuth,
+      deps: [AuthService],
+      multi: true,
+    },
   ],
 };
