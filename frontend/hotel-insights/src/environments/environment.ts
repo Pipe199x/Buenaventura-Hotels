1b3e9f7fc4ca10540e56902c101c12ca7a21795a
Pipe199x <duqueandres800@gmail.com>
2026-07-16 14:25:20 -0500

    Update Supabase URL in environment configuration

--- frontend/hotel-insights/src/environments/environment.ts
+++ frontend/hotel-insights/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://auth.buenaventuradatos.com',
+  supabaseUrl: 'https://rxdsgkkwfbbfnthusunr.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4ZHNna2t3ZmJiZm50aHVzdW5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNDY5ODAsImV4cCI6MjA3NDcyMjk4MH0.lGQ2i1_DB9bqqPZUTItuchJV8OtYD7ipI43m7B0oCOc'
 };
 
